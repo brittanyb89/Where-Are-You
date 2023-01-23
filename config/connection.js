aac2bf4c989e12471efaf8e504ae1b2d5eb93504
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-22 18:32:30 -0600

    Set up connections in config folder

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+require("dotenv").config();
+
+const connectionProps = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
+
+const connection = mysql.createConnection(connectionProps);
+
+module.exports = {
+  connectionProps,
+  connection,
+};
